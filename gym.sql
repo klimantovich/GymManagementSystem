3e0ce100be2fd81014b44fb5a67406124d11c006
vitali <vitali@gmail.com>
2024-02-13 23:10:40 +0300

    make initial changes

--- gym.sql
+++ gym.sql
@@ -1,22 +1,66 @@
-CREATE TABLE info(username VARCHAR(200), password VARCHAR(500), name VARCHAR(100), prof INT, street VARCHAR(100), city VARCHAR(50), phone VARCHAR(32), PRIMARY KEY(username));
+CREATE TABLE IF NOT EXISTS `info`
+(
+	`username` VARCHAR(200), 
+	`password` VARCHAR(500), 
+	`name` VARCHAR(100), 
+	`prof` INT, 
+	`street` VARCHAR(100), 
+	`city` VARCHAR(50), 
+	`phone` VARCHAR(32),
+	`time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+	PRIMARY KEY(`username`)
+);
 
---CREATE TABLE members(username VARCHAR(200), plan VARCHAR(100), trainor VARCHAR(200), FOREIGN KEY(username) references info(username), PRIMARY KEY(username), FOREIGN KEY(trainor) references info(username));
+CREATE TABLE IF NOT EXISTS `plans`
+(
+	`name` VARCHAR(100),
+	`exercise` VARCHAR(100),
+	`reps` INT,
+	`sets` INT,
+	PRIMARY KEY(`name`)
+);
 
-CREATE TABLE plans(name VARCHAR(100), PRIMARY KEY(name));
+CREATE TABLE IF NOT EXISTS `receps`
+(
+	`username` VARCHAR(200), 
+	PRIMARY KEY(`username`), 
+	FOREIGN KEY(`username`) references `info`(`username`)
+);
 
-CREATE TABLE receps(username VARCHAR(200), PRIMARY KEY(username), FOREIGN KEY(username) references info(username));
+CREATE TABLE IF NOT EXISTS `trainors`
+(
+	`username` VARCHAR(200), 
+	PRIMARY KEY(`username`), 
+	FOREIGN KEY(`username`) references `info`(`username`)
+);
 
-CREATE TABLE trainors(username VARCHAR(200), PRIMARY KEY(username), FOREIGN KEY(username) references info(username));
+CREATE TABLE IF NOT EXISTS `members`
+(
+	`username` VARCHAR(200), 
+	`plan` VARCHAR(100), 
+	`trainor` VARCHAR(200), 
+	PRIMARY KEY(`username`), 
+	FOREIGN KEY(`username`) references `info`(`username`), 
+	FOREIGN KEY(`plan`) references `plans`(`name`), 
+	FOREIGN KEY(`trainor`) references `trainors`(`username`)
+);
 
-CREATE TABLE members(username VARCHAR(200), plan VARCHAR(100), trainor VARCHAR(200), PRIMARY KEY(username), FOREIGN KEY(username) references info(username), FOREIGN KEY(plan) references plans(name), FOREIGN KEY(trainor) references trainors(username));
+CREATE TABLE IF NOT EXISTS `progress`
+(
+	`username` VARCHAR(200), 
+	date DATE, `daily_result` VARCHAR(200), 
+	time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+	PRIMARY KEY(`username`, date), 
+	FOREIGN KEY(`username`) references `members`(`username`)
+);
 
-ALTER TABLE info ADD time TIMESTAMP DEFAULT CURRENT_TIMESTAMP;--done for all tables
+CREATE TABLE IF NOT EXISTS `equip`
+(
+	`name` VARCHAR(100),
+	`count` INT,
+	PRIMARY KEY(`name`)
+);
 
-INSERT INTO info(username, password, name, prof, street, city, phone) VALUES('eswar_123', '$5$rounds=535000$ajR8hAzSoSF.NhEs$MaLn1dbnXq9eu2W5Ge3c1ScAS9960yLBFv3aU9zaxc0', 'Parameswar K', 1, 'Adarshnagar', 'Anantapur', 9666585361);--admin's password is eswar@259522
-
-CREATE TABLE progress(username VARCHAR(200), date DATE, daily_result VARCHAR(200), time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(username, date), FOREIGN KEY(username) references members(username));
-
-
-INSERT INTO info(username, password, street, city, phone, name, prof)
- VALUES('eswar_123', '$5$rounds=535000$6gsmZKME5DrojTtI$8WcFkNyq0vGAh7M2splCCf6ZSVDcG3xOEDWP5XBRNL2', 
- 	'Adarshnagar', 'Anantapur', '9666585361', 'Parameswar Kurakula', 1);
+REPLACE INTO `info`(`username`, `password`, `name`, `prof`, `street`, `city`, `phone`) 
+VALUES('admin', '$5$rounds=535000$jlqSxaic6j1Omgdz$Pz/QfrzJP/iNNU36l.1Wm37P.5MD8M7i7I0MVj4e1E5', 
+'DB Admin', 1, 'Some avenue 1', 'Chicago', 375258121178);
